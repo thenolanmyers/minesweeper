fd62ddd4dcb7bed0171087418959f9ae67ced0f9
Nolan Myers <nolanmyers@NSM-Home.local>
2017-10-19 15:19:57 -0700

    Show blank board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,12 @@
 const blankLine = '  |   |  ';
 
+// Show blank board
 console.log('This is what an empty board would look like:');
+console.log(`${blankLine}\n${blankLine}\n${blankLine}`);
+
+// Show guess board
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(`${guessLine}\n${bombLine}\n${blankLine}`);
