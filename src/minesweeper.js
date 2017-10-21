93daf584f4cfe0a2563a180efe41f5ed0a33e8ea
Nolan Myers <nolanmyers@gmail.com>
2017-10-20 23:42:26 -0700

    Generate player and bomb boards; refactor printBoard.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,56 @@
+const generatePlayerBoard = (rows, columns) => {
+  return generateBoard (rows, columns, ' ');
+}
+
+function generateBoard (rows, columns, filler) {
+  if (rows <= 0 || columns <= 0) {
+    console.log ("Error generating board");
+    return undefined;
+  }
+  let board = [];
+  for (i = 0; i < rows; i++) {
+    let row = [];
+    for (j=0; j < columns; j++ ) {
+      row.push(filler);
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+const generateBombBoard = (rows, columns, numBombs) => {
+  let board = generateBoard(rows, columns, ' ');
+
+  let bombsPlaced = 0;
+  while (bombsPlaced < numBombs) {
+      let randRow = Math.floor(Math.random() * rows);
+      let randCol = Math.floor(Math.random() * columns);
+      board[randRow][randCol] = 'B';
+      // console.log('Placed ' + randRow + ', ' + randCol);
+      bombsPlaced++;
+      // TODO: refactor to handle case where one bomb is placed on top of another
+  }
+  return board;
+}
+
+/*
+const printBoard = (board) => {
+  console.log("Current board:");
+  for (const row of board) {
+    console.log(row.join(' | '));
+  }
+};
+*/
+
+const printBoard = (header, board) => {
+  console.log(header);
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+printBoard('Player board:', generatePlayerBoard(3,4));
+printBoard('Bomb board:', generateBombBoard(3,4,5));
+
+/*
 // create and initialize board
 const board = [];
 for(i = 0; i < 3; i++) {
@@ -6,16 +59,13 @@ for(i = 0; i < 3; i++) {
     board[i].push(' ');
   }
 }
+*/
 
-const printBoard = (board) => {
-  console.log("Current board:");
-  for (const row of board) {
-    console.log(row.join(' | '));
-  }
-};
 
+/*
 printBoard(board);
 
 board[0][1] = 1;
 board[2][2] = 'B';
 printBoard(board);
+*/
