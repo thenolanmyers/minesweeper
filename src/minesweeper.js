eee4e9e3ac4b04f74fe9d4f1624a0fbb1b5f4598
Nolan Myers <nolanmyers@gmail.com>
2017-10-23 16:27:10 -0700

    Add getNumberOfNeighborBombs function and debug mode in printBoard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,7 +2,7 @@ const generatePlayerBoard = (rows, columns) => {
   return generateBoard (rows, columns, ' ');
 }
 
-function generateBoard (rows, columns, filler) {
+const generateBoard = (rows, columns, filler) => {
   if (rows <= 0 || columns <= 0) {
     console.log ("Error generating board");
     return undefined;
@@ -19,24 +19,91 @@ function generateBoard (rows, columns, filler) {
 };
 
 const generateBombBoard = (rows, columns, numBombs) => {
-  let board = generateBoard(rows, columns, null);
+  let board = generateBoard(rows, columns, ' ');
+
+  if (numBombs > rows * columns) {
+    console.log ('Too many bombs to place.');
+    return board;
+  }
 
   let bombsPlaced = 0;
   while (bombsPlaced < numBombs) {
       let randRow = Math.floor(Math.random() * rows);
       let randCol = Math.floor(Math.random() * columns);
 
-      board[randRow][randCol] = 'B';
-      bombsPlaced++;
-      // TODO: refactor to handle case where one bomb is placed on top of another
+      // This is SUPER INEFFICIENT if bomb density gets high
+      // TODO fix this to pull from a "deck" of random locations
+      if (board[randRow][randCol] !== 'B') {
+        board[randRow][randCol] = 'B';
+        bombsPlaced++;
+      }
   }
   return board;
 }
 
-const printBoard = (header, board) => {
-  console.log(header);
+const getNumberOfNeighborBombs = (board, row, col) => {
+  const neighborOffsets = [];
+
+  // Add a neighbor for each row and column +/- 1 from index
+  for (i=-1; i<=1; i++) {
+    for (j=-1; j<=1; j++){
+      if (i === 0 && j === 0) continue;
+
+      neighborOffsets.push([row + i, col + j]);
+    }
+  }
+
+  // Count bombs among all offsets
+  let bombCount = 0;
+  neighborOffsets.forEach( offset => {
+    const i = offset[0];
+    const j = offset[1];
+
+    // But only if in range of the board
+    if ( i >= 0 && i < board.length && j >= 0 && j < board[0].length ) {
+      console.log(`Checking ${i}, ${j}`);
+      if (board[i][j] === 'B') bombCount++;
+    }
+  });
+
+  // Print the board in debug mode
+  board[row][col] = 'X';
+  printBoard ('Count elements: ', board, true);
+  console.log(`Count for ${row}, ${col} = ${bombCount}`);
+
+  return bombCount;
+}
+
+const printBoard = (title, board, debugMode) => {
+  // If in debugMode add a header row and column to the board before printing
+  // Header row will be on top; header column on the right side (easier to read than left side)
+  if (debugMode) {
+    // Create a header row and prepend it to the board
+    const rowHead = [];
+    for (i=0; i<board[0].length; i++) {
+      rowHead.push(i);
+    }
+    board.unshift (rowHead);
+
+    // Add a header column to each row on the board
+    let rowCounter = -1; // accommodate the header row
+    board.forEach( row => {
+      row.push (rowCounter++);
+    });
+  }
+
+  // Now print the board
+  console.log(title);
   console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-printBoard('Player board:', generatePlayerBoard(3,4));
-printBoard('Bomb board:', generateBombBoard(3,4,5));
+
+/* MAIN BODY OF PROGRAM */
+
+const playerBoard = generatePlayerBoard(6,5);
+const bombBoard = generateBombBoard(6,5,12);
+
+printBoard('Player board:', playerBoard);
+printBoard('Bomb board:', bombBoard);
+
+console.log(getNumberOfNeighborBombs(bombBoard,3,2));
